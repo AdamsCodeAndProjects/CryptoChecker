880064fc3b4e9a14f112c3417b87306523534c24
Adam Januszewski <Adpjan@att.net>
2022-05-27 22:16:49 -0400

    Initial Commit

---
+++ CryptThree/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptThree
+//
+//  Created by adam janusewski on 5/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptThree/CryptThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptThreeApp.swift
+//  CryptThree
+//
+//  Created by adam janusewski on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
